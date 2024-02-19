c73005e42e6bcd61612492917c32c76b97f053ae
Jealous Matsikachando <pasej5@gmail.com>
2024-02-19 20:18:47 +0200

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,3 +5,20 @@ Declerative Mapping
 
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class User(Base):
+    """
+    Declarative Mapping
+    
+    Args:
+        Base(_type_): _description_
+    """
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
