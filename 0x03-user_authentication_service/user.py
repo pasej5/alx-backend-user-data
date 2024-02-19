4efea15ea46308f5b3fe0ea3c983c6051dead04b
Jealous Matsikachando <pasej5@gmail.com>
2024-02-19 20:21:51 +0200

    pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,15 +8,16 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class User(Base):
     """
     Declarative Mapping
-    
+
     Args:
         Base(_type_): _description_
     """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
