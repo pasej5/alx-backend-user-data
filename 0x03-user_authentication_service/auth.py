f8b73498bb4530a1a4e44c7f4c86847a2931a3e2
Jealous Matsikachando <pasej5@gmail.com>
2024-02-21 02:36:30 +0200

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,8 +23,7 @@ def _hash_password(password: str) -> bytes:
     Returns:
         bytes: description
     """
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 class Auth:
