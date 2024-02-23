3cbf9fd94a5cd4f07c210075f4068a8d1cf83521
Jealous Matsikachando <pasej5@gmail.com>
2024-02-23 04:27:55 +0200

    retieve the user session ide from cookies and then use it to get information from the server then delete

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,5 +52,17 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """_summary_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
